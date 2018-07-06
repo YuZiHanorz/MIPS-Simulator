a00f5889ad536f1ceaede722d0eae615eb236940
zhyu18 <38875982+YuZiHanorz@users.noreply.github.com>
2018-07-06 22:31:38 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,37 +3,34 @@
 using namespace std;
 
 /*int main() {
-	int cnt = 1;
 	Parser parser;
-	ifstream inf("53.s");
+	ifstream inf("12.s");
 	parser.parse(inf);
 	int next_line = parser.label_text["main"];
 	regi[34] = next_line;
-	ofstream out("ans.txt");
+	pc = regi[34];
+	//ofstream out("ans.txt");
 	while (next_line < parser.operation.size()) {
 		Token tmp = parser.operation[next_line];
 		exec_token(tmp, next_line, memory, memPos);
 		regi[34] = next_line;
-		out << tmp;
-		out << "display" << (cnt++) << '\n';
+		cout << tmp;
+		cout << "display" << (cnt++) << '\n';
 		for (int i = 0; i < 35; ++i)
-			out << i << ':' << regi[i] << ' ';
-		out << '\n';
+			cout << regi[i] << ' ';
+		cout << '\n';
 	}
-
 	while (true) {
-		Instruction_Fetch(parser);
-		if (pc - 1 >= parser.operation.size())
-			break;
-		Instruction_Decode();
-		Execution();
-		Memory_Access();
+		cyc++;
 		Write_Back();
-		out << if_id;
-		out << "display" << (cnt++) << '\n';
-		for (int i = 0; i < 35; ++i)
-			out << i << ':' << regi[i] << ' ';
-		out << '\n';
+		Memory_Access();
+		Execution();
+		Instruction_Decode();
+		Instruction_Fetch(parser);
+		if (ed) 
+			edcnt--;
+		if (edcnt == 0)
+			break;
 	}
 
 	return 0;
@@ -45,16 +42,19 @@ int main(int argc, char *argv[]) {
 	Parser parser;
 	parser.parse(inf);
 	int next_line = parser.label_text["main"];
-	regi[34] = next_line;
+	pc = regi[34] = next_line;
 
 	while (true) {
-		Instruction_Fetch(parser);
-		if (pc - 1 >= parser.operation.size())
-			break;
-		Instruction_Decode();
-		Execution();
-		Memory_Access();
+		cyc++;
 		Write_Back();
+		Memory_Access();
+		Execution();
+		Instruction_Decode();
+		Instruction_Fetch(parser);
+		if (ed)
+			edcnt--;
+		if (edcnt == 0)
+			break;
 	}
 
 	inf.close();
